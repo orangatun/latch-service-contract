27d0f6e8e8c9b166d7f9cdac572d51ce2dc9d732
orangatun <raghunadhan@protonmail.com>
2023-11-23 16:49:53 -0800

    Mentions working

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import { Block } from './components/Block';
 
 import './App.css';
 
-
-// TO-DO remove marks being passed everywhere. It's been added to styling.
-
 function App() {
 
+  console.log(input);
   let blocksList = [];
   let marks = {bold: false, italics: false, underline: false};
+  
   input.forEach((block, index)=> {
     blocksList.push(Block(block, index, marks));
-  })
+  });
   return (
     blocksList
-  )
+  );
 }
 
 export default App;
